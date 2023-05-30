403542f13a5ec7ee452e99c1fe0e6205f3ed286f
oyesaurav <78659500+oyesaurav@users.noreply.github.com>
2023-05-30 21:23:44 +0530

    added Cls_2 for classification

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -20,19 +20,45 @@ import {
   ColorSchemeProvider,
   Header,
   ActionIcon,
+  Alert,
 } from "@mantine/core"
 import { IconClick, IconMoonStars, IconSun } from "@tabler/icons-react"
 
-const elements = [
-  { label: "Label1", score: 0.8942, some: "C", others: "Carbon" },
-  { label: "Label2", score: 0.8942, some: "C", others: "Carbon" },
-  { label: "Label3", score: 0.8942, some: "C", others: "Carbon" },
-  { label: "Label4", score: 0.8942, some: "C", others: "Carbon" },
-  { label: "Label5", score: 0.8942, some: "C", others: "Carbon" },
+const classifiers = [
+  {
+    name: "Cls_1",
+    description: "Using Multi layer perceptron to predict the medical specialty of the clinical notes",
+    avail: "Will be added soon",
+  },
+  {
+    name: "Cls_2",
+    description: "Fine tuned DistilBert model on our dataset to predict the medical specialty of the clinical notes",
+    avail: "Available",
+  },
+  {
+    name: "Cls_3",
+    description: "Bert feature extraction -> [CLS] hidden state -> 9 hidden linear layers",
+    avail: "Will be added soon",
+  },
+  {
+    name: "Cls_4",
+    description: "Bert feature extraction -> 512 states concatenated -> 9 hidden linear layers",
+    avail: "Will be added soon",
+  },
+  {
+    name: "Cls_5",
+    description: "Bert feature extraction -> All hidden states -> GRU net/ BiGRUnet",
+    avail: "Will be added soon",
+  },
+  {
+    name: "Cls_6",
+    description: "Bert feature extraction -> All hidden states ->  Conv 1D + Capsule Network (decoder used GRU)",
+    avail: "Will be added soon",
+  },
 ]
 
 export default function Home() {
-  const [color, setColor] = useState("light")
+  const [color, setColor] = useState("dark")
   const theme = useMantineTheme()
   const classes = useStyles()
   const PRIMARY_COL_HEIGHT = rem(400)
@@ -40,25 +66,39 @@ export default function Home() {
   const CUSTOM_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 10 - ${theme.spacing.md} / 6)`
   const [file, setFile] = useState(null)
   const [text, setText] = useState("")
-  const [model, setModel] = useState("Ens")
+  const [model, setModel] = useState("Cls_2")
+  const [rows, setRows] = useState()
 
   const ths = (
     <tr>
-      <th>Label</th>
+      <th>Medical_specialty</th>
       <th>Score</th>
-      <th>Some</th>
-      <th>Others</th>
+      {/* <th>Some</th>
+      <th>Others</th> */}
     </tr>
   )
 
-  const rows = elements.map((element) => (
-    <tr key={element.label}>
-      <td>{element.label}</td>
-      <td>{element.score}</td>
-      <td>{element.some}</td>
-      <td>{element.others}</td>
-    </tr>
-  ))
+  async function Cls_2(data) {
+    try {
+      const response = await fetch("https://api-inference.huggingface.co/models/oyesaurav/dwellbert", {
+        headers: { Authorization: process.env.NEXT_PUBLIC_HUGGING_FACE },
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+      const result = await response.json()
+      setRows(
+        result[0].slice(0, 5).map((element) => (
+          <tr key={element.label}>
+            <td>{element.label}</td>
+            <td>{element.score}</td>
+          </tr>
+        ))
+      )
+    }
+    catch (error) {
+      Alert({ title: "Error", message: "Model hasn't loaded, please try again", color: "red" })
+    }
+  }
 
   return (
     <div>
@@ -99,6 +139,7 @@ export default function Home() {
                 autosize
                 minRows={15}
                 maxRows={18}
+                onChange={(event) => setText(event.currentTarget.value)}
               />
               <Grid>
                 <Grid.Col span={8}>
@@ -119,7 +160,12 @@ export default function Home() {
                   )}
                 </Grid.Col>
                 <Grid.Col span={4}>
-                  <Button leftIcon={<IconClick size="1rem" />} loading={false} fullWidth>
+                  <Button
+                    leftIcon={<IconClick size="1rem" />}
+                    loading={false}
+                    fullWidth
+                    onClick={() => Cls_2({ inputs: text })}
+                  >
                     Submit
                   </Button>
                 </Grid.Col>
@@ -136,15 +182,20 @@ export default function Home() {
                   size="md"
                   value={model}
                   onChange={setModel}
-                  data={["Ens", "Bert_1", "Distilbert_1", "MLP", "xyz"]}
+                  data={["Cls_1", "Cls_2", "Cls_3", "Cls_4", "Cls_5", "Cls_6"]}
                   classNames={classes}
                 />
+                <Text fw={500}>{
+                  classifiers[model.split("_")[1]-1].description                
+                }</Text>
               </Grid.Col>
               <Grid.Col>
                 {/* <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} /> */}
                 <Table highlightOnHover withBorder fullWidth>
                   <thead>{ths}</thead>
-                  <tbody>{rows}</tbody>
+                  <tbody>{
+                  classifiers[model.split("_")[1]-1].avail === "Available" ? rows : <tr><td>The model will be added</td></tr>
+                  }</tbody>
                 </Table>
               </Grid.Col>
             </Grid>
